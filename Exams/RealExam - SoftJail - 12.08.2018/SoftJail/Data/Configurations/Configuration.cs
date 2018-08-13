adf950ef3269969b239ea55512712447d9697b0c
melik <melikpehlivanov@gmail.com>
2018-08-13 13:05:08 +0300

    Deleted SoftJail

--- Exams/RealExam - SoftJail - 12.08.2018/SoftJail/Data/Configurations/Configuration.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace SoftJail.Data.Configurations
-{
-   public static class Configuration
-    {
-        public static string ConnectionString = @"Server=.;Database=SoftJail;Trusted_Connection=True";
-    }
-}
--- Exams/RealExam - SoftJail - 12.08.2018/SoftJail/DataProcessor/ExportDto/Prisoner/PrisonerOfficerDto.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace SoftJail.DataProcessor.ExportDto.Prisoner
-{
-    public class PrisonerOfficerDto
-    {
-        public string OfficerName { get; set; }
-
-        public string Department { get; set; }
-    }
-}
